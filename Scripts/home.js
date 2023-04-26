d543655c0e58c2b96118d6ec268eb18273731b7a
CashmereKey <dhruvsethi870@gmail.com>
2023-04-26 18:26:46 +0530

    Day_1 UI

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,34 +1,58 @@
-var button = document.getElementById('toggle-button');
-console.log(button);
-
-
-
-
-// $(document).ready(function () {
-//   var trigger = $('.hamburger'),
-//       overlay = $('.overlay'),
-//      isClosed = false;
-
-//     trigger.click(function () {
-//       hamburger_cross();      
-//     });
-
-//     function hamburger_cross() {
-
-//       if (isClosed == true) {          
-//         overlay.hide();
-//         trigger.removeClass('is-open');
-//         trigger.addClass('is-closed');
-//         isClosed = false;
-//       } else {   
-//         overlay.show();
-//         trigger.removeClass('is-closed');
-//         trigger.addClass('is-open');
-//         isClosed = true;
-//       }
-//   }
-  
-//   $('[data-toggle="offcanvas"]').click(function () {
-//         $('#wrapper').toggleClass('toggled');
-//   });  
-// });
+var ctx_1 = document.getElementById('myChart_1').getContext('2d');
+var ctx_2 = document.getElementById('myChart_2').getContext('2d');
+var myChart = new Chart(ctx_1, {
+    type: 'doughnut',
+    data: {
+        // labels: ['Blue'],
+        datasets: [{
+            data: [100,30],
+            backgroundColor: [   
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(54, 162, 235, 0.8)'                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'               
+            ],
+            borderWidth: 1,
+            
+        }]
+    },
+    options: {
+        cutoutPercentage: 30,
+        legend: {
+            display: true,
+            position: 'top'
+        },
+        responsive: false,
+        maintainAspectRatio: true
+    }
+});
+var myChart = new Chart(ctx_2, {
+    type: 'doughnut',
+    data: {
+        // labels: ['Blue'],
+        datasets: [{
+            data: [100,70],
+            backgroundColor: [   
+                'rgba(255, 20, 132, 0.8)',
+                'rgba(154, 160, 235, 0.8)'                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'               
+            ],
+            borderWidth: 1,
+            
+        }]
+    },
+    options: {
+        cutoutPercentage: 30,
+        legend: {
+            display: true,
+            position: 'top'
+        },
+        responsive: false,
+        maintainAspectRatio: true
+    }
+});

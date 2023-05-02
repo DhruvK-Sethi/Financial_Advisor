bc668a684ad5b11f1d601cd650863e23088abdf4
CashmereKey <dhruvsethi870@gmail.com>
2023-05-02 21:56:44 +0530

    Changes...

--- app.py/app.py
+++ app.py/app.py
@@ -2,16 +2,22 @@ from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
 import hashlib
 import secrets
+from Crypto.Cipher import AES
+import os
 
 app = Flask(__name__)
 app.secret_key = "mysecretkey"
 
+
 @app.route("/")
 def landing():
-	return render_template("landing.html")
+    return render_template("landing.html")
 
 
+privatekey = os.urandom(32)
 # Signup function
+
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     msg = None
@@ -19,8 +25,14 @@ def signup():
         username = request.form["username"]
         password = request.form["password"]
         if username and password:
-            privatekey = secrets.token_hex(16)
-            password_hashed = hashlib.sha256(password.encode()).hexdigest()
+            cipher = AES.new(privatekey, AES.MODE_EAX)
+
+            ciphertext, tag = cipher.encrypt_and_digest(password)
+
+            # Get the nonce and update the cipher object
+            nonce = cipher.nonce
+            cipher = AES.new(privatekey, AES.MODE_EAX, nonce=nonce)
+            password_hashed = cipher
             conn = sqlite3.connect("signup.db")
             c = conn.cursor()
             conn.execute('''CREATE TABLE IF NOT EXISTS personsdata
@@ -28,16 +40,20 @@ def signup():
                   username TEXT NOT NULL,
                   password TEXT NOT NULL,
                   privatekey TEXT NOT NULL);''')
-            conn.execute("INSERT INTO personsdata (username, password, privatekey) VALUES (?, ?, ?)", (username, password_hashed, privatekey))
+            conn.execute("INSERT INTO personsdata (username, password, privatekey) VALUES (?, ?, ?)",
+                         (username, password_hashed, privatekey))
             conn.commit()
             conn.close()
-            msg = "You have successfully signed up! Your private key is: {}".format(privatekey)
+            msg = "You have successfully signed up! Your private key is: {}".format(
+                privatekey)
         else:
             msg = "Please enter both username and password"
     return render_template("signup.html", msg=msg)
 
 
 # Login function
+
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     msg = None
@@ -47,7 +63,8 @@ def login():
         if username and privatekey:
             conn = sqlite3.connect("signup.db")
             c = conn.cursor()
-            c.execute("SELECT * FROM personsdata WHERE username = ? AND privatekey = ?", (username, privatekey))
+            c.execute(
+                "SELECT * FROM personsdata WHERE username = ? AND privatekey = ?", (username, privatekey))
             r = c.fetchone()
             conn.close()
             if r:
@@ -67,18 +84,20 @@ def logout():
     session.pop("username", None)
     return redirect(url_for("home"))
 
+
 @app.route("/dashboard")
 def dashboard():
-	if "loggedin" in session:
-		conn = sqlite3.connect("signup.db")
-		c = conn.cursor()
-		c.execute("SELECT privatekey FROM personsdata WHERE username = ?", (session["username"],))
-		privatekey = c.fetchone()[0]
-		conn.close()
-		return render_template("dashboard.html", username=session["username"], privatekey=privatekey)
-	else:
-		return redirect(url_for("login"))
-	
+    if "loggedin" in session:
+        conn = sqlite3.connect("signup.db")
+        c = conn.cursor()
+        c.execute("SELECT privatekey FROM personsdata WHERE username = ?",
+                  (session["username"],))
+        privatekey = c.fetchone()[0]
+        conn.close()
+        return render_template("dashboard.html", username=session["username"], privatekey=privatekey)
+    else:
+        return redirect(url_for("login"))
+
 # @app.route('/fill_details', methods=['GET', 'POST'])
 # def fill_details():
 #     if request.method == 'POST':
@@ -89,19 +108,20 @@ def dashboard():
 #                          email TEXT NOT NULL,
 #                          phone INTEGER NOT NULL,
 #                          age INTEGER NOT NULL,
-                         
+
 #                          monthlyexpense INTEGER NOT NULL);''')
 #         name = request.form['name']
 #         email = request.form['email']
 #         phone = request.form['phone']
 #         age = request.form['age']
-        
+
 #         monthlyexpense = request.form['monthlyexpense']
 #         conn.execute("INSERT INTO user_details (name, email, phone, age, salary, monthlyexpense) VALUES (?, ?, ?, ?, ?, ?)", (name, email, phone, age, monthlyexpense))
 #         conn.commit()
 #         conn.close()
 #         return redirect('/dashboard')
 #     return render_template('fill_details.html')
+
 
 @app.route('/fill_details', methods=['GET', 'POST'])
 def fill_details():
@@ -121,7 +141,8 @@ def fill_details():
         age = request.form['age']
         monthlyexpense = request.form['monthlyexpense']
         retirementvalue = int(monthlyexpense) * 12 * 25
-        conn.execute("INSERT INTO user_details (name, email, phone, age, monthlyexpense, retirementvalue) VALUES (?, ?, ?, ?, ?, ?)", (name, email, phone, age, monthlyexpense, retirementvalue))
+        conn.execute("INSERT INTO user_details (name, email, phone, age, monthlyexpense, retirementvalue) VALUES (?, ?, ?, ?, ?, ?)",
+                     (name, email, phone, age, monthlyexpense, retirementvalue))
         conn.commit()
         conn.close()
         return redirect('/retirement/{}'.format(retirementvalue))
@@ -132,20 +153,11 @@ def fill_details():
 def retirement(retirementvalue):
     return render_template('retirement.html', retirementvalue=retirementvalue)
 
+
 @app.route('/asset_allocation')
 def asset_allocation():
     return render_template(url_for('asset_allocation.html'))
 
 
-
-
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-	
-
-
-
-
-
